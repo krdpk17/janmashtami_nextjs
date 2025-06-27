eb6a1b649b10fbebb822df1a38c6ada224e0041f
Deepak Kumar <53008098+venlite@users.noreply.github.com>
2025-06-27 10:09:36 +0530

    feat: create hello world next.js app

---
+++ hello-world-nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', minHeight: '100vh', alignItems: 'center', justifyContent: 'center', fontSize: '2rem' }}>
+      Hello World
+    </main>
+  );
+}
